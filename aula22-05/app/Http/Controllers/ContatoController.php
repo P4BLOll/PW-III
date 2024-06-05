45780d0c6bc690ce2012f2f33ad245b083ea39cf
P4BLOll <pablosousa386@gmail.com>
2024-06-05 14:34:56 -0300

    Atualizado

--- aula22-05/app/Http/Controllers/ContatoController.php
+++ aula22-05/app/Http/Controllers/ContatoController.php
@@ -7,12 +7,41 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function cadastrar(Request $req){
-        $contato = new Contato();
+    public function index(Request $req){
+        $contato = Contato::all();
+        return view('index')->with("contato", $contato);
+    }
+
+    public function adicionar(Request $req){
+        $contato = new Contato;
         $contato->nome = $req->nome;
         $contato->telefone = $req->telefone;
         $contato->email = $req->email;
         $contato->save();
+        return redirect()->back();
+    }
+
+    public function editar(Request $req){
+        $contato = Contato::find($req->id);
+        return view('editar')->with("contato", $contato);
+    }
+
+    public function atualizar(Request $req){
+        $contato = Contato::find($req->id);
+        $contato->update(
+            [
+                "nome" => $req->nome,
+                "telefone" => $req->telefone,
+                "email" => $req->email
+            ]
+        );
+        return redirect()->back();
+        
+    }
+
+    public function excluir(Request $req){
+        $contato = Contato::find($req->id);
+        $contato->delete();
         return redirect()->back();
     }
 }
--- aula22-05/routes/web.php
+++ aula22-05/routes/web.php
@@ -8,3 +8,8 @@ Route::get('/', function () {
 });
 
 Route::post('/cadastrar', [ContatoController::class, 'cadastrar']);
+Route::get("/", [ContatoController::class, "index"]);
+Route::post("/adicionar", [ContatoController::class, "adicionar"]);
+Route::get("/editar/{id}", [ContatoController::class, "editar"]);
+Route::post("/atualizar/{id}", [ContatoController::class, "atualizar"]);
+Route::get("/excluir/{id}", [ContatoController::class, "excluir"]);
